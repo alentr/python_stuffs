02eb7e6f832f1c3f5add275d3ac078d7780feec6
Alexandre Telles <alexandre.ntr@gmail.com>
2024-01-07 23:40:40 +0000

    configure_intellij

--- configure_intellij/setup_nr_local.py
+++ configure_intellij/setup_nr_local.py
@@ -1,13 +1,13 @@
 import xml.etree.ElementTree as ET
 
-envs = [
-    'NEW_RELIC_AGENT_ENABLED',
-    'NEW_RELIC_APP_ID',
-    'NEW_RELIC_APP_NAME',
-    'NEW_RELIC_LICENSE_KEY'
-]
+envs = {
+    "NEW_RELIC_AGENT_ENABLED": "true",
+    "NEW_RELIC_APP_ID": "1491436248",
+    "NEW_RELIC_APP_NAME": "calls-router-web",
+    "NEW_RELIC_LICENSE_KEY": "123"
+}
 
-xml_path = 'workspace.xml'
+xml_path = '../workspace.xml'
 app_name = 'CallsRouterApplicationKt'
 
 
@@ -39,12 +39,16 @@ if config_element is not None:
     if envs_element is None:
         envs_element = ET.SubElement(config_element, "envs")
 
-    for env_to_remove in envs:
-        env = envs_element.find(f".//env[@name='{env_to_remove}']")
-        envs_element.remove(env)
+    for env in envs:
+        # Create a new env element
+        new_env = ET.Element('env', {'name': env, 'value': envs[env]})
+
+        # Append the new env element to the envs_element
+        envs_element.append(new_env)
 
     # Convert the modified tree back to a string
     modified_xml = ET.tostring(xml, encoding='utf-8').decode('utf-8')
+
     save_xml_to_file(modified_xml, xml_path)
 else:
     print(f"Error: Configuration '{app_name}' not found in the XML.")
